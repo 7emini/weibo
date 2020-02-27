3ec4c038d5d36b73776c584f633b35ac6bd5a456
GaoHe <touchmain@qq.com>
2020-02-27 17:06:29 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Weibo') - Laravel 入门项目</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+                <li class="nav-item"><a class="nav-link" href="/help">登录</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('title', 'Home')
 @section('content')
-<h1>Home</h1>
+<div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+        你现在看到的是<a href="#">Laravel6 入门教程</a> 的示例项目主页。
+    </p>
+    <p>
+        一切，将从这里开始
+    </p>
+    <p>
+        <a href="btn btn-lg btn-success" href="#">现在注册</a>
+    </p>
+</div>
 @endsection
