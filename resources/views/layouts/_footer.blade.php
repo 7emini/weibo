ba53976fbf2f23ce333c4eb136fac34ce7ae3f0f
GaoHe <touchmain@qq.com>
2020-02-27 17:26:49 +0800

    分割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <img class="brand-icon" src="/logo.png" alt="">
+    <a href="#">刻意练习，每日精进</a>
+
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
