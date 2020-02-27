b784e9405b6df96c50158832f2d5125999ac1d70
GaoHe <touchmain@qq.com>
2020-02-27 17:31:29 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="footer">
     <img class="brand-icon" src="/logo.png" alt="">
-    <a href="#">刻意练习，每日精进</a>
+    <a href="{{ route('home') }}">刻意练习，每日精进</a>
 
     <div class="float-right">
-        <a href="/about">关于</a>
+        <a href="{{ route('about') }}">关于</a>
     </div>
 </footer>
